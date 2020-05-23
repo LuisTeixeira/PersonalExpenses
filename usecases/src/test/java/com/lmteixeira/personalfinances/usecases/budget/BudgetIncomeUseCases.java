d7b89376869e11d9b9f84b034156fc5f5eefc468
Luis Teixeira <teixeira13@gmail.com>
2020-05-23 18:38:45 +0200

    Adds exception tests for adding income and getting income count

--- usecases/src/test/java/com/lmteixeira/personalfinances/usecases/budget/BudgetIncomeUseCases.java
+++ usecases/src/test/java/com/lmteixeira/personalfinances/usecases/budget/BudgetIncomeUseCases.java
@@ -1,6 +1,7 @@
 package com.lmteixeira.personalfinances.usecases.budget;
 
 import com.lmteixeira.personalfinances.usecases.config.TestConfig;
+import com.lmteixeira.personalfinances.usecases.exceptions.BudgetNotFoundException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,6 +29,28 @@ public class BudgetIncomeUseCases {
         addIncome.addIncome(utils.getUserEmail(), "Test Income", BigDecimal.valueOf(23d));
         Long incomeCount = getIncomeCount.getIncomeCount(utils.getUserEmail());
         Assert.assertEquals(Long.valueOf(1), incomeCount);
+    }
+
+    @Test
+    public void addingIncomeShouldThrowExceptionWhenThereIsNoBudgetCreatedForUserWithSpecifiedEmail() {
+        boolean exceptionThrown = false;
+        try {
+            addIncome.addIncome(utils.getUserEmail(), "Test Income", BigDecimal.valueOf(20d));
+        } catch (BudgetNotFoundException e) {
+            exceptionThrown = true;
+        }
+        Assert.assertTrue(exceptionThrown);
+    }
+
+    @Test
+    public void getIncomeCountShouldThrowAnExceptionWhenThereIsNoBudgetForUserWithSpecifiedEmail() {
+        boolean exceptionThrown = false;
+        try {
+            Long count = getIncomeCount.getIncomeCount(utils.getUserEmail());
+        } catch (BudgetNotFoundException ex) {
+            exceptionThrown = true;
+        }
+        Assert.assertTrue(exceptionThrown);
     }
 
 }
